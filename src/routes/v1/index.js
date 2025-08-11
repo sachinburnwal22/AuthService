a8f17243f99f173398c931f80d3b51c88500206a
SACHIN BURNWAL <sachinburnwal22@gmail.com>
2025-08-11 10:55:38 +0530

    Added isAuth function

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -12,4 +12,9 @@ router.post(
   AuthRequestValidators.validateUserAuth,
   UserController.signIn
 );
+router.get("/isAuthenticated", UserController.isAuthenticated);
+
+router.get("/dummy", (req, res) => {
+  return res.status(200).json({ message: "OK" });
+});
 module.exports = router;
